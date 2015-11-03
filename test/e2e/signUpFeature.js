c4a98ad1952695cb56a336cb8a6ffa97dcc01583
ana <adscarlos@gmail.com>
2015-11-03 14:40:41 +0000

    adds karma and protractor basic config

---
+++ test/e2e/signUpFeature.js
@@ -0,0 +1,7 @@
+describe('User Sign up page', function() {
+  it('has a title', function() {
+    browser.get('http://localhost:8080');
+
+    expect(browser.getTitle()).toEqual('leftovers');
+  });
+});
