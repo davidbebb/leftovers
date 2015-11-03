9c352aabe4e49312984554d3d5a56aa147457dc6
Lucy Fang <lucyfang2@gmail.com>
2015-11-03 15:25:18 +0000

    testing big_oven api

--- app.js
+++ app.js
@@ -2,22 +2,44 @@ var LeftOvers = angular.module('leftOvers', []);
 
 LeftOvers.controller('LeftOversCtrl', function($scope, $http) {
 
-    $scope.test = "hello";
+  var self = this;
+  var apiKey = "9m9bjXUNhUIE78Lf26Yby9bV5UE4X7";
+  var recipeId = "/recipes?title_kw=chicken&include_ing=onion,oil&exclude_ing=raisin&pg=1&rpp=5";
+  var url = "http://api.bigoven.com" + recipeId + "&api_key=" + apiKey;
+  var recipe_url = 'http://api.bigoven.com/recipe/63033' + "?api_key=" + apiKey
 
 
-      var apiKey = "9m9bjXUNhUIE78Lf26Yby9bV5UE4X7zi";
-      // var recipeId = 196149;
-      var recipeId = "/recipes?title_kw=chicken&include_ing=onion,oil&exclude_ing=raisin&pg=1&rpp=20";
-      // var url = "http://api.bigoven.com/recipe/" + recipeId + "?api_key=" + apiKey;
-      var url = "http://api.bigoven.com" + recipeId + "&api_key=" + apiKey;
+  $http.get(url).
+    success(function(data, status, headers, config) {
+      $scope.posts = data.Results;
+    }).
+      error(function(data, status, headers, config) {
+        console.log("try again");
+    });
 
 
-      $http.get(url).
-        success(function(data, status, headers, config) {
-          $scope.posts = data.Results;
-        }).
-          error(function(data, status, headers, config) {
-            console.log("try again");
-        });
+  $http.get(recipe_url).
+    success(function(data, status, headers, config) {
+      $scope.chicken_satay = data;
+      $scope.chicken_satay_i = data.Ingredients;
+    }).
+      error(function(data, status, headers, config) {
+        console.log("try again");
+    });
+
+
+
+  // $scope.star_rating = function("#rating") {
+  //   console.log("#rating")
+  //   Math.round("#rating")
+      //  remainder = (5 - )
+  //
+  // }
+
+        // def star_rating(rating)
+        //   return rating unless rating.respond_to?(:round)
+        //   remainder = (5 - rating)
+        //   '★' * rating.round + '☆' * remainder
+        // end
 
 })
