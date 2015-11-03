e266d023bad5fbaa79a0f9084fdbc09d5796ce4e
davidbebb <davidbebb@gmail.com>
2015-11-03 15:24:59 +0000

    Angular app started. First test now passes

---
+++ app.module.js
@@ -0,0 +1,1 @@
+var leftoversApp = angular.module('LeftoversApp', []);
--- test/e2e/signUpFeature.js
+++ test/e2e/signUpFeature.js
@@ -1,7 +1,6 @@
 describe('User Sign up page', function() {
   it('has a title', function() {
     browser.get('http://localhost:8080');
-
     expect(browser.getTitle()).toEqual('leftovers');
   });
 });
