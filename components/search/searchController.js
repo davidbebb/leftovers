be4a4ed0f5ac3f750ae30ad5452d292431bf766d
MoeSadoon <msadoon590@gmail.com>
2015-11-04 16:13:08 +0000

    able to add ingredient but not able to stop duplicates

--- components/search/searchController.js
+++ components/search/searchController.js
@@ -1,1 +1,19 @@
-search.controller('SearchController', function($scope) {
+leftoversApp.controller('SearchController', function($scope) {
+  var ref = new Firebase("https://leftovers-app.firebaseio.com/users");
+
+  $scope.ingredients = ['test_ingredient'];
+
+  $scope.addIngredient = function(ingredient){
+    // for (var i = 0; i < $scope.ingredients.length; i++) {
+    //    if ($scope.ingredients[i] !== ingredient) {
+
+        $scope.ingredients.push(ingredient);
+        console.log($scope.ingredients);
+        // $scope.ingredient.push()
+      // };
+
+    // };
+
+
+  };
+});
