724af06f73b1c25e3eecb7d8df1037e18611a18f
Lucy Fang <lucyfang2@gmail.com>
2015-11-04 12:20:35 +0000

    sorting our merge conflicts

---
+++ test/unit/facebookAuthSpec.js
@@ -0,0 +1,14 @@
+describe('FacebookAuthController', function() {
+  beforeEach(module('LeftoversApp'));
+
+  var ctrl;
+
+  beforeEach(inject(function($controller) {
+    ctrl = $controller('FacebookAuthController');
+  }));
+
+  it('', function() {
+    expect(ctrl.login).toBeDefined();
+  });
+
+});
