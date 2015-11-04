ba08de04fe56e56e0ad42035118968405ee7a3b8
davidbebb <davidbebb@gmail.com>
2015-11-04 10:34:06 +0000

    facebook button working

--- components/signUp/facebookAuthController.js
+++ components/signUp/facebookAuthController.js
@@ -1,3 +1,12 @@
-leftoversApp.controller('FacebookAuthController', function() {
-
+leftoversApp.controller('FacebookAuthController', function($scope) {
+  var ref = new Firebase("https://leftovers-app.firebaseio.com/");
+  $scope.login= function(){
+    ref.authWithOAuthPopup("facebook", function(error, authData) {
+    if (error) {
+      console.log("Login Failed!", error);
+    } else {
+      console.log("Authenticated successfully with payload:", authData);
+    }
+  });
+  };
 });
