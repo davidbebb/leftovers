13543c8358d40b75ef37985be263c1c5d2f16fe1
davidbebb <davidbebb@gmail.com>
2015-11-05 15:16:52 +0000

    trying to access elements in facebook login page

--- test/e2e/signUpFeature.js
+++ test/e2e/signUpFeature.js
@@ -32,43 +32,4 @@ describe('User Sign up page', function() {
     expect(myElement.getText()).toBeFalsy();
   });
 
-  it('should login succesfully using auth0', function() {
-    browser.get('http://localhost:8080');
-    var loginButton = element(by.id('signup'));
-    loginButton.click();
-
-    browser.getAllWindowHandles().then(function(handles) {
-      browser.switchTo().window(handles[1]).then(function() {
-        expect(browser.getTitle()).toEqual('facebook');
-
-
-        //wait for pop-up fields to be displayed (they are on the page but might be hidden initially)
-        browser.driver.sleep(2000);
-
-        //type credentials and click the 'access' button to log in
-        var emailField = browser.driver.findElement(by.id('email'));
-        expect(emailField.isPresent()).toBeTruthy();
-        emailField.sendKeys('open_qdmtykh_user@tfbnw.net');
-        var passWordField = browser.driver.findElement(by.id('pass'));
-        passWordField.sendKeys('leftovers');
-        var accessButton = browser.driver.findElement(by.id('u_0_2'));
-        accessButton.click();
-      });
-
-      browser.switchTo().window(handles[0]).then(function() {
-        expect(browser.getTitle()).toEqual('leftovers');
-        // var myElement = element(by.id('loggedInUsername'));
-        // expect(myElement.isPresent()).toBeTruthy();
-        // // expect(myElement.getText()).to.eventually.contain("Open Graph Test User");
-        // expect(myElement.getText()).toBeTruthy();
-
-
-        // callback();
-      });
-
-    });
-  });
-
-
-
 });
