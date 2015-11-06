41192c76a996c164b1c72c890cad841d7744071c
MoeSadoon <msadoon590@gmail.com>
2015-11-06 10:41:00 +0000

    cleaned up facebook auth test

--- test/e2e/faceBookLogin.js
+++ test/e2e/faceBookLogin.js
@@ -6,17 +6,16 @@ describe('User Sign up page', function() {
   var password = $('#pass')
   var loginBtn = $('#u_0_2')
 
-  it('should login succesfully using auth0', function() {
+  beforeEach(function () {
+
     browser.get('http://localhost:8080');
     var loginButton = element(by.id('signup'));
     loginButton.click();
 
-    expect(browser.getTitle()).toEqual('leftovers');
 
     browser.getAllWindowHandles().then(function(handles) {
       browser.switchTo().window(handles[1]).then(function() {
         browser.ignoreSynchronization = true;
-        expect(browser.driver.getTitle()).toEqual('Facebook');
         email.waitReady();
         email.sendKeys('open_qdmtykh_user@tfbnw.net');
         password.waitReady();
@@ -28,9 +27,14 @@ describe('User Sign up page', function() {
 
       browser.switchTo().window(handles[0]).then(function() {
         browser.ignoreSynchronization = false;
-        var myElement = element(by.id('displayName'));
-        expect(myElement.getText()).toEqual('Open Graph Test User');
       });
     });
   });
+
+  it('should login succesfully using auth0', function() {
+    expect(browser.getTitle()).toEqual('leftovers');
+    var myElement = element(by.id('displayName'));
+    expect(myElement.getText()).toEqual('Open Graph Test User');
+  });
+
 });
