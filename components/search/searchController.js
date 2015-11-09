bc424a4c937abb41925d6788353e7378232452f4
Lucy Fang <lucyfang2@gmail.com>
2015-11-09 22:47:30 +0000

    users now adding and can add some favorites data

--- components/search/searchController.js
+++ components/search/searchController.js
@@ -1,128 +1,131 @@
-leftoversApp.controller('SearchController', function($scope, $http,
-                        ingredientLists) {
+leftoversApp.controller('SearchController', ['$scope', '$rootScope', '$http', 'ingredientLists',
+  function($scope, $rootScope, $http, ingredientLists) {
 
-  $scope.ingredients = [];
-  $scope.posts = null;
-  $scope.excluded = [];
-
-  var apiKey = '';
-  var ref = new Firebase('https://leftovers-app.firebaseio.com/users');
-  var apiRef = new Firebase('https://leftovers-app.firebaseio.com/secrets');
-
-  apiRef.once('value', function(data) {
-    return apiKey = data.val();
-  });
-
-  $scope.diet = {
-    option: '',
-  };
-
-  $scope.clearDiet = function() {
-    $scope.diet.option = '';
-  };
-
-  $scope.getDietID = function() {
-    var dietUrl = '&cuisine=' + $scope.diet.option;
-    return dietUrl;
-  };
-
-  // $scope.getList = function(file) {
-  //   ingredientLists.fetch(file).then(function(data) {
-  //     $scope.ingredientPreset = data;
-  //   });
-  //   console.log('getList ' + file);
-  // };
-
-  ingredientLists.fetch('meat').then(function(data) {
-    $scope.meat = data;
-  });
-
-  ingredientLists.fetch('veg').then(function(data) {
-    $scope.veg = data;
-  });
-
-  ingredientLists.fetch('fruits').then(function(data) {
-    $scope.fruits = data;
-  });
-
-  ingredientLists.fetch('seafood').then(function(data) {
-    $scope.seafood = data;
-  });
-
-  ingredientLists.fetch('dairy').then(function(data) {
-    $scope.dairy = data;
-  });
-
-  ingredientLists.fetch('baking_grain').then(function(data) {
-    $scope.grains = data;
-  });
-
-  $scope.getIngredients = function() {
-    return $scope.ingredients;
-  };
-
-  $scope.check = function(value, checked) {
-    var index = $scope.ingredients.indexOf(value);
-    if (index >= 0 && !checked) {
-      $scope.ingredients.splice(index, 1);
-    }
-
-    if (index < 0 && checked) {
-      $scope.ingredients.push(value);
-    }
-  };
-
-  $scope.url = function() {
-    var dietOpt = $scope.getDietID();
-    var excludedOpt = '&exclude_ing=' + $scope.excluded.toString();
-    var recipeOpt = '/recipes?any_kw=' + ($scope.ingredients).toString();
-    var searchUrl = 'http://api.bigoven.com' + recipeOpt + dietOpt +
-                    excludedOpt + '&api_key=' + apiKey + '&pg=1&rpp=15';
-    return searchUrl;
-  };
-
-  $scope.addIngredient = function(ingredient, exclusion) {
-    $scope.ingredients.push(ingredient);
-    $scope.excluded.push(exclusion);
-    var url = $scope.url();
-
-    $http.get(url).
-      success(function(data, status, headers, config) {
-        $scope.posts = data.Results;
-      }).
-        error(function(data, status, headers, config) {
-      });
-
-    $scope.ingredient = '';
     $scope.ingredients = [];
-    $scope.excluded = [];
-    console.log(url);
-  };
+    $scope.posts = null;
+    $scope.excludedIngredients = [];
 
-  $scope.getRecipe = function(recipeID) {
-    console.log(recipeID);
-    var recipeUrl = 'http://api.bigoven.com/recipe/' + recipeID + '?api_key=' +
-                    apiKey;
+    var apiKey = '';
+    var fbID = '';
+    var ref = new Firebase('https://leftovers-app.firebaseio.com/users');
+    var apiRef = new Firebase('https://leftovers-app.firebaseio.com/secrets');
+    var userRef = new Firebase('https://leftovers-app.firebaseio.com/users/' + $rootScope.fbID);
 
-    $http.get(recipeUrl).
-      success(function(data, status, headers, config) {
-        $scope.recipe = data;
-        $scope.recipeIngredients = data.Ingredients;
-        console.log($scope.recipe);
-      }).
-        error(function(data, status, headers, config) {
-      });
-
-    console.log(recipeUrl);
-  };
-
-  $scope.addFavorite = function(title, ingredients, instructions) {
-    console.log(title);
-    ref.push({
-      title: title,
-      ingredients: ingredients,
-      instructions: instructions,
+    apiRef.once('value', function(data) {
+      return apiKey = data.val();
     });
-  };
 
-});
+    $scope.diet = {
+      option: '',
+    };
+
+    $scope.clearDiet = function() {
+      $scope.diet.option = '';
+    };
+
+    $scope.getDietID = function() {
+      var dietUrl = '&cuisine=' + $scope.diet.option;
+      return dietUrl;
+    };
+
+    // $scope.getList = function(file) {
+    //   ingredientLists.fetch(file).then(function(data) {
+    //     $scope.ingredientPreset = data;
+    //   });
+    //   console.log('getList ' + file);
+    // };
+
+    ingredientLists.fetch('meat').then(function(data) {
+      $scope.meat = data;
+    });
+
+    ingredientLists.fetch('veg').then(function(data) {
+      $scope.veg = data;
+    });
+
+    ingredientLists.fetch('fruits').then(function(data) {
+      $scope.fruits = data;
+    });
+
+    ingredientLists.fetch('seafood').then(function(data) {
+      $scope.seafood = data;
+    });
+
+    ingredientLists.fetch('dairy').then(function(data) {
+      $scope.dairy = data;
+    });
+
+    ingredientLists.fetch('baking_grain').then(function(data) {
+      $scope.grains = data;
+    });
+
+    $scope.getIngredients = function() {
+      return $scope.ingredients;
+    };
+
+    $scope.check = function(value, checked) {
+      var index = $scope.ingredients.indexOf(value);
+      if (index >= 0 && !checked) {
+        $scope.ingredients.splice(index, 1);
+      }
+
+      if (index < 0 && checked) {
+        $scope.ingredients.push(value);
+      }
+    };
+
+    $scope.url = function() {
+      var dietOpt = $scope.getDietID();
+      var excludedOpt = '&exclude_ing=' + $scope.excludedIngredients.toString();
+      var recipeOpt = '/recipes?any_kw=' + ($scope.ingredients).toString();
+      var searchUrl = 'http://api.bigoven.com' + recipeOpt + dietOpt +
+                      excludedOpt + '&api_key=' + apiKey + '&pg=1&rpp=15';
+      return searchUrl;
+    };
+
+    $scope.addIngredient = function(ingredient, excluded) {
+      $scope.ingredients.push(ingredient);
+      $scope.excludedIngredients.push(excluded);
+      var url = $scope.url();
+
+      $http.get(url).
+        success(function(data, status, headers, config) {
+          $scope.posts = data.Results;
+        }).
+          error(function(data, status, headers, config) {
+        });
+
+      $scope.ingredient = '';
+      $scope.ingredients = [];
+      $scope.excluded = '';
+      $scope.excludedIngredients = [];
+      console.log(url);
+    };
+
+    $scope.getRecipe = function(recipeID) {
+      console.log(recipeID);
+      var recipeUrl = 'http://api.bigoven.com/recipe/' + recipeID + '?api_key=' +
+                      apiKey;
+
+      $http.get(recipeUrl).
+        success(function(data, status, headers, config) {
+          $scope.recipe = data;
+          $scope.recipeIngredients = data.Ingredients;
+          console.log($scope.recipe);
+        }).
+          error(function(data, status, headers, config) {
+        });
+
+      console.log(recipeUrl);
+    };
+
+    $scope.addFavorite = function(title, ingredients, instructions) {
+      console.log(title);
+      userRef.set({
+        title: title,
+        //here need to get all the data of the selected recipe
+        // ingredients: ingredients,
+        // instructions: instructions
+      });
+    };
+}]);
--- components/signUp/facebookAuthController.js
+++ components/signUp/facebookAuthController.js
@@ -1,43 +1,49 @@
-leftoversApp.controller('FacebookAuthController', function($scope) {
+leftoversApp.controller('FacebookAuthController', ['$scope', '$rootScope',
+  function($scope, $rootScope) {
 
-  var ref = new Firebase('https://leftovers-app.firebaseio.com/users');
-  //var ref = new Firebase("https://leftovers-app.firebaseio.com/");
+    var ref = new Firebase('https://leftovers-app.firebaseio.com/users');
 
-  $scope.login = function() {
-    ref.authWithOAuthPopup('facebook', function(error, authData) {
-      if (error) {
-        console.log('Login Failed!', error);
+    $scope.login = function() {
+      ref.authWithOAuthPopup('facebook', function(error, authData) {
+        if (error) {
+          console.log('Login Failed!', error);
+        } else {
+          console.log('Authenticated successfully with payload');
+        }
+
+        location.reload();
+      });
+    };
+
+    ref.onAuth(function(authData) {
+      var isLoggedIn = false;
+      if (authData === null) {
+        isLoggedIn = false;
+        console.log('Not logged in yet', isLoggedIn);
       } else {
-        console.log('Authenticated successfully with payload:', authData);
+        isLoggedIn = true;
+        console.log('Logged in as', authData.facebook.displayName, isLoggedIn);
+
+        ref.once('value', function(snapshot) {
+          console.log(snapshot.val());
+          key = authData.facebook.id;
+          if (snapshot.child(key).exists()) {
+            console.log('it is a match!');
+          } else {
+            console.log('should add this');
+            ref.child(key).set({authData});
+          }
+        });
       }
-      key = authData.facebook.id;
-      userRef = ref.child(key).set({test: '3'});
 
-      // var key = authData;
-      // ref.push({key});
-      location.reload();
+      $scope.authData = authData;
+      $scope.isLoggedIn = isLoggedIn;
+      $rootScope.fbID = authData.facebook.id;
+
     });
-  };
 
-  ref.onAuth(function(authData) {
-    var isLoggedIn = false;
-    if (authData === null) {
-      isLoggedIn = false;
-      console.log('Not logged in yet', isLoggedIn);
-    } else {
-      isLoggedIn = true;
-      console.log('Logged in as', authData.uid, isLoggedIn);
-
-    }
-
-    $scope.authData = authData;
-    $scope.isLoggedIn = isLoggedIn;
-
-  });
-
-  $scope.logout = function() {
-    ref.unauth();
-    location.reload();
-  };
-
-});
+    $scope.logout = function() {
+      ref.unauth();
+      location.reload();
+    };
+}]);
